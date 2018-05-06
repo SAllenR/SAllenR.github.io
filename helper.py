33c031dfd3151ee8931a1ce4ed14dd68cb19d882
sallenr <sr.832.878.2572@gmail.com>
2018-05-06 01:28:13 -0500

    from python

--- helper.py
+++ helper.py
@@ -1,8 +1,6 @@
 import subprocess
 
-msg = 'from python'
-
 print (subprocess.check_output('git init', shell=True))
 
-print (subprocess.check_output('git commit -a -m msg', shell=True))
+print (subprocess.check_output('git commit -a -m "from python"', shell=True))
 print (subprocess.check_output('git push origin master', shell=True))
