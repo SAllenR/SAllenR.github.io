a2362724bc2c85c78f620575379ac262f1dce36e
sallenr <sr.832.878.2572@gmail.com>
2018-05-06 01:20:51 -0500

    a commit

---
+++ helper.py
@@ -0,0 +1,6 @@
+import subprocess
+
+print (subprocess.check_output('git init', shell=True))
+print (subprocess.check_output('git add .', shell=True))
+print (subprocess.check_output('git commit -m "a commit"', shell=True))
+print (subprocess.check_output('git push origin master', shell=True))
